a31506cfff54636e2274bec2598d8618dc8668b6
Daniel Mendes de Almeida <daniel.m.almeida.97@gmail.com>
2023-03-16 15:58:35 -0300

    Router
    Instalando e configurando o Router

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './Content.css'
+import './Content.css';
+import { Routes, Route } from "react-router-dom";
+import About from "../../views/examples/About"
+import Home from "../../views/examples/Home"
 
 const Content = props => (
-    <div className='Content'>
-        Componente Content
-    </div>
+    <main className='Content'>
+        <Routes>
+            <Route path="/about" element={<About/>} />
+            {/* <Route path="/param/:id" element={<Param />} /> */}
+            <Route path="/" exact element={<Home />} />
+            {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+    </main>
 )
 
-export default Content
+export default Content;
--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Menu.css'
+import { Link } from 'react-router-dom';
+
 
 const Menu = props => (
-    <div className='Menu'>
-        Componente Menu
-    </div>
+    <aside className='Menu'>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Início</Link>
+                </li>
+                <li>
+                    <Link to="/about">Sobre</Link>
+                </li>
+            </ul>
+        </nav>
+    </aside>
 )
 
 export default Menu
